7c0a56ab71e6d7bc6b3f8ed909f92e1f16762ab1
Sasha Komkova <sasha1komkova@gmail.com>
2026-03-11 17:33:59 +0300

    made app and classes

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import product, Category
 
-# Register your models here.
+admin.site.register(product)
+admin.site.register(Category)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField("Наименивание", max_length=225, null=False, blank=False)
+    parent_category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.CASCADE, verbose_name="Категория")
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.title
+class product(models.Model):
+    title = models.CharField("Наименивание", max_length=225, null=False, blank=False)
+    cost = models.DecimalField("Цена", max_digits=11, decimal_places=2, null=False, blank=False)
+    description = models.TextField("Описание", null=False, blank=False)
+    category = models.ForeignKey("Category", null=False, blank=False, on_delete=models.CASCADE, verbose_name="Категория")
+    image_url = models.URLField("Изображение", max_length=225, null=False, blank=False)
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+    
+    def __str__(self):
+        return self.title
