f8956d8724c61b519ca9570d6ef7ce9358d36fbd
Sasha Komkova <sshakmkva@gmail.com>
2026-03-16 16:29:47 +0300

    оформление HTML-страницы

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from app.models import product
 
-# Create your views here.
+def index(request):
+    products = product.objects.all()
+    context = {
+        'products':products
+    }
+    return render(request, 'app/index.html', context)
